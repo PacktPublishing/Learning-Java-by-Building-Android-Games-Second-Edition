330e70991ca6048a9e7739b55119036757948c19
MatthewLanduyt <mattlanduyt@gmail.com>
2023-11-14 05:10:39 +0000

    Delete Chapter21 directory

--- Chapter21/java/MovementComponent.java
+++
@@ -1,8 +0,0 @@
-package com.gamecodeschool.c21scrollingshooter;
-
-interface MovementComponent {
-
-    boolean move(long fps,
-                 Transform t,
-                 Transform playerTransform);
-}
