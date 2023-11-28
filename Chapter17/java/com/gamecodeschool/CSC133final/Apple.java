31f64c342e4b37ec8bbd4ad98a46a69937166830
VTWalker <vinnythai.29@gmail.com>
2023-11-27 23:15:26 -0800

    Builder Design for Apple class
    Implemented the builder design pattern for the Apple class

--- Chapter17/java/com/gamecodeschool/CSC133final/Apple.java
+++ Chapter17/java/com/gamecodeschool/CSC133final/Apple.java
@@ -8,25 +8,59 @@ import android.graphics.Paint;
 import android.graphics.Point;
 import java.util.Random;
 
-class Apple extends GameObject{
+public class Apple {
 
+    private Point location = new Point();
+    private Bitmap mBitmapApple;
 
-    /// Set up the apple in the constructor
-    Apple(Context context, Point sr, int s){
-        super(context, sr, s);
-        // Load the image to the bitmap
-        mbitmapobject = BitmapFactory.decodeResource(context.getResources(), drawable.);
-        //Matt here. R Seems to be an undefined value. FIX? Apple resource in drawable table. Will have to ask professor later.
-
-        // Resize the bitmap
-        mbitmapobject = Bitmap.createScaledBitmap(mbitmapobject, s, s, false);
+    // Private constructor to enforce the use of the builder
+    private Apple(Point spawnRange, int size, Bitmap bitmapApple) {
+        location.x = -10; // Hide the apple off-screen until the game starts
+        mBitmapApple = bitmapApple;
+        mBitmapApple = Bitmap.createScaledBitmap(mBitmapApple, size, size, false);
     }
 
     // This is called every time an apple is eaten
-    void spawn(){
-        // Choose two random values and place the apple
+    void spawn(Point spawnRange) {
         Random random = new Random();
-        location.x = random.nextInt(mRange.x) + 1;
-        location.y = random.nextInt(mSpawnRange.y - 1) + 1;
+        location.x = random.nextInt(spawnRange.x) + 1;
+        location.y = random.nextInt(spawnRange.y - 1) + 1;
+    }
+
+    // Let SnakeGame know where the apple is
+    // SnakeGame can share this with the snake
+    Point getLocation() {
+        return location;
+    }
+
+    // Draw the apple
+    void draw(Canvas canvas, Paint paint) {
+        canvas.drawBitmap(mBitmapApple, location.x * mBitmapApple.getWidth(), location.y * mBitmapApple.getHeight(), paint);
+    }
+
+    // Builder class for Apple
+    public static class AppleBuilder {
+        private Point mSpawnRange;
+        private int mSize;
+        private Bitmap mBitmapApple;
+
+        public AppleBuilder setSpawnRange(Point spawnRange) {
+            mSpawnRange = spawnRange;
+            return this;
+        }
+
+        public AppleBuilder setSize(int size) {
+            mSize = size;
+            return this;
+        }
+
+        public AppleBuilder setBitmap(Context context, int resourceId) {
+            mBitmapApple = BitmapFactory.decodeResource(context.getResources(), resourceId);
+            return this;
+        }
+
+        public Apple build() {
+            return new Apple(mSpawnRange, mSize, mBitmapApple);
+        }
     }
 }
