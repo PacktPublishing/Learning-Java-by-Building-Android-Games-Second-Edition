a3fb678163a92890712dd13252967bbf8daa5c53
VTWalker <vinnythai.29@gmail.com>
2023-12-04 11:07:26 -0800

    Merging report 2 properly
    Merging report 2 to the master branch that we forked from, instead of the parent repository we pulled from.

--- Chapter17/java/Apple.java
+++ Chapter17/java/Apple.java
@@ -8,56 +8,59 @@ import android.graphics.Paint;
 import android.graphics.Point;
 import java.util.Random;
 
-class Apple {
+public class Apple {
 
-    // The location of the apple on the grid
-    // Not in pixels
     private Point location = new Point();
-
-    // The range of values we can choose from
-    // to spawn an apple
-    private Point mSpawnRange;
-    private int mSize;
-
-    // An image to represent the apple
     private Bitmap mBitmapApple;
 
-    /// Set up the apple in the constructor
-    Apple(Context context, Point sr, int s){
-
-        // Make a note of the passed in spawn range
-        mSpawnRange = sr;
-        // Make a note of the size of an apple
-        mSize = s;
-        // Hide the apple off-screen until the game starts
-        location.x = -10;
-
-        // Load the image to the bitmap
-        mBitmapApple = BitmapFactory.decodeResource(context.getResources(), R.drawable.apple);
-
-        // Resize the bitmap
-        mBitmapApple = Bitmap.createScaledBitmap(mBitmapApple, s, s, false);
+    // Private constructor to enforce the use of the builder
+    private Apple(Point spawnRange, int size, Bitmap bitmapApple) {
+        location.x = -10; // Hide the apple off-screen until the game starts
+        mBitmapApple = bitmapApple;
+        mBitmapApple = Bitmap.createScaledBitmap(mBitmapApple, size, size, false);
     }
 
     // This is called every time an apple is eaten
-    void spawn(){
-        // Choose two random values and place the apple
+    void spawn(Point spawnRange) {
         Random random = new Random();
-        location.x = random.nextInt(mSpawnRange.x) + 1;
-        location.y = random.nextInt(mSpawnRange.y - 1) + 1;
+        location.x = random.nextInt(spawnRange.x) + 1;
+        location.y = random.nextInt(spawnRange.y - 1) + 1;
     }
 
     // Let SnakeGame know where the apple is
     // SnakeGame can share this with the snake
-    Point getLocation(){
+    Point getLocation() {
         return location;
     }
 
     // Draw the apple
-    void draw(Canvas canvas, Paint paint){
-        canvas.drawBitmap(mBitmapApple,
-                location.x * mSize, location.y * mSize, paint);
-
+    void draw(Canvas canvas, Paint paint) {
+        canvas.drawBitmap(mBitmapApple, location.x * mBitmapApple.getWidth(), location.y * mBitmapApple.getHeight(), paint);
     }
 
+    // Builder class for Apple
+    public static class AppleBuilder {
+        private Point mSpawnRange;
+        private int mSize;
+        private Bitmap mBitmapApple;
+
+        public AppleBuilder setSpawnRange(Point spawnRange) {
+            mSpawnRange = spawnRange;
+            return this;
+        }
+
+        public AppleBuilder setSize(int size) {
+            mSize = size;
+            return this;
+        }
+
+        public AppleBuilder setBitmap(Context context, int resourceId) {
+            mBitmapApple = BitmapFactory.decodeResource(context.getResources(), resourceId);
+            return this;
+        }
+
+        public Apple build() {
+            return new Apple(mSpawnRange, mSize, mBitmapApple);
+        }
+    }
 }
