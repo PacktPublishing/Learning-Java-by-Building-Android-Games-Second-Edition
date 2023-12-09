6100dc7bd7de815ab2e5195353e78acc011ad0b1
MatthewLanduyt <mattlanduyt@gmail.com>
2023-12-08 20:20:48 -0800

    Revert "Add files via upload"
    This reverts commit fabc1db88849143cff17f304e7a1bca24e2be818.

--- Audio.java
+++
@@ -1,8 +0,0 @@
-package com.gamecodeschool.csc133finalproject;
-
-// added the audio class for strategy implementation
-public interface Audio {
-    void playEatSound();
-
-    void playCrashSound();
-}
